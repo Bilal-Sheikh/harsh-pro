6d6cc43f7694b91f12918c405de92e33df57e7b4
Bilal Sheikh <72bilal.sheikh@gmail.com>
2024-01-20 14:52:45 +0530

    changed import

--- src/three.js
+++ src/three.js
@@ -1,77 +1,83 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+// import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json";
 
 window.onload = async () => {
-  const canvas = document.querySelector(".webgl");
+    const canvas = document.querySelector(".webgl");
 
-  // Scene
+    // Scene
 
-  const scene = new THREE.Scene();
+    const scene = new THREE.Scene();
 
-  // Geometry
+    // Geometry
 
-  const loader = new FontLoader();
+    const loader = new FontLoader();
 
-  loader.load(
-    "../static/helvetiker_regular.typeface.json",
-      function (font) {
-      const geometry = new TextGeometry("Hello three.js!", {
-        font: font,
-        size: 0.5,
-        height: 0.2,
-        curveSegments: 5,
-        bevelEnabled: true,
-        bevelThickness: 0.03,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 4,
-      });
+    loader.load(
+        "../static/helvetiker_regular.typeface.json",
+        // typefaceFont,
+        function (font) {
+            const geometry = new TextGeometry("Hello three.js!", {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 4,
+            });
 
-      geometry.center();
+            geometry.center();
 
-      const material = new THREE.MeshNormalMaterial();
+            const material = new THREE.MeshNormalMaterial();
 
-      const text = new THREE.Mesh(geometry, material);
-      scene.add(text);
+            const text = new THREE.Mesh(geometry, material);
+            scene.add(text);
+        }
+    );
+
+    // Material
+
+    // const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'green' })
+
+    // Mesh
+
+    // const mesh = new THREE.Mesh(cube, cubeMaterial)
+    // scene.add(mesh)
+
+    // Camera
+
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight
+    );
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // Renderer
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Controls
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        controls.update();
+        renderer.render(scene, camera);
     }
-  );
 
-  // Material
-
-  // const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'green' })
-
-  // Mesh
-
-  // const mesh = new THREE.Mesh(cube, cubeMaterial)
-  // scene.add(mesh)
-
-  // Camera
-
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
-  camera.position.z = 3;
-  scene.add(camera);
-
-  // Renderer
-
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-  });
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  // Controls
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true;
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    controls.update();
-    renderer.render(scene, camera);
-  }
-
-  animate();
+    animate();
 };
